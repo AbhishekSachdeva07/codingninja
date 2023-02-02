3f2926df797eb99735965d2a4175e6b48efdb3e6
AbhishekSachdeva07 <abhisachdeva711@gmail.com>
2023-02-02 17:53:14 +0530

    Create intro.c

---
+++ Introduction to C/intro.c
@@ -0,0 +1,11 @@
+//average
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter 3 no.\n");
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+    printf("The average of no. is %d",(a+b+c)/3);
+}
